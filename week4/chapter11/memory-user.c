0567398b1f69c0befe0299d92718d43f93959b60
LJiaqingNick <gmziue@gmail.com>
2022-09-30 19:58:55 -0700

    add get pid

--- week4/chapter11/memory-user.c
+++ week4/chapter11/memory-user.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #define MEGABYTE 1048576
 
 int main(int argc, char* argv[]) {
+	int pid_t = getpid();
+	printf("Current process id is %d\n", pid_t);
 	int totalMemory = atoi(argv[1]) * MEGABYTE;
 	int* array = malloc(totalMemory);
 	if (array == NULL) {
