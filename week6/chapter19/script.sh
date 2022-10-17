22405292aab88ef257c41a56f8fe6450fd803b85
LJiaqingNick <gmziue@gmziue.com>
2022-10-17 01:02:48 -0400

    update the script

--- week6/chapter19/script.sh
+++ week6/chapter19/script.sh
@@ -1,8 +1,9 @@
 #!bin/bash
+rm data.txt
 gcc -Wall -O0 tlb.c -o tlb
 num_page=1
-max_pages=4000
-num_trials=100000
+max_pages=1500
+num_trials=1000000
 while [ $num_page -lt $max_pages ]
 do
   ./tlb $num_page $num_trials | tee -a data.txt
